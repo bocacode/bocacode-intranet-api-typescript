e1949d6586ac6ecac7dbc827a3a5d1d3c4c4e486
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-03-09 21:35:42 -0500

    change news schema, link instead of url

--- src/models/newsModel.ts
+++ src/models/newsModel.ts
@@ -6,7 +6,7 @@ const NewsSchema = new mongoose.Schema(
     author: { type: String, required: true },
     title: { type: String, required: true },
     summary: { type: String, required: true },
-    url: { type: String, required: true },
+    link: { type: String, required: true },
     created_by: { type: String, required: true },
     enabled: { type: Boolean },
     modified_by: { type: String },
