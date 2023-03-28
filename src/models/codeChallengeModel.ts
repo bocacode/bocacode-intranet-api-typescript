7edbafaa0612ce284753a134c8919a61741d8683
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-03-28 14:36:24 -0400

    update challenges data

--- src/models/codeChallengeModel.ts
+++ src/models/codeChallengeModel.ts
@@ -3,11 +3,10 @@ import mongoose from 'mongoose'
 const CodeChallengeSchema = new mongoose.Schema(
   {
     uid: { type: String, required: true },
-    media_type: { type: String, required: true },
     topic: { type: String, required: true },
+    difficulty_level: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
-    url: { type: String, required: true },
     created_by: { type: String, required: true },
     enabled: { type: Boolean },
     modified_by: { type: String },
