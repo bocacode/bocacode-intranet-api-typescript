87294bd80aa044eea71132bf2c8031be505b201c
jonathans199 <jso1919@gmail.com>
2023-04-18 17:12:33 -0400

    updated seeder data and function

--- src/scripts/seedDb.ts
+++ src/scripts/seedDb.ts
@@ -1,12 +1,34 @@
 import mongoose, { ConnectOptions } from 'mongoose'
 import 'dotenv/config'
 
-import Restaurant from '../models/restaurantModel'
-import Homework from '../models/homeworkModel'
-import Tutorial from '../models/tutorialModel'
-
-import { dummyRestaurants, dummyHomework, dummyTutorials } from './seedData'
 import { addLog } from '../controllers/logController'
+
+import RestaurantModel from '../models/restaurantModel'
+const restaurantData = require('../../old_data/restaurants.json')
+
+import TutorialModal from '../models/tutorialModel'
+const tutorialsData = require('../../old_data/tutorials.json')
+
+import HomeworkModal from '../models/homeworkModel'
+const homeworkData = require('../../old_data/homeworks.json')
+
+import JobsModal from '../models/jobModel'
+const jobsData = require('../../old_data/jobs.json')
+
+import LabsModal from '../models/labModel'
+const labsData = require('../../old_data/labs.json')
+
+import LectureModel from '../models/lectureModel'
+const lecturesData = require('../../old_data/lectures.json')
+
+import NewsModel from '../models/newsModel'
+const newsData = require('../../old_data/news.json')
+
+import EventModel from '../models/eventModel'
+const eventsData = require('../../old_data/events.json')
+
+import ChallengeModel from '../models/codeChallengeModel'
+const challengesData = require('../../old_data/challenges.json')
 
 mongoose.set('strictQuery', false)
 mongoose
@@ -26,35 +48,34 @@ mongoose
   })
   .catch((err) => console.error(err))
 
-const seedDB = async (data: any, type: string) => {
+const seedDB = async (data: any, type: any, name: string) => {
   try {
     // await Restaurant.deleteMany({})  // enable only to delete all items in collection
-
-    // const dummyDataCreated = await Homework.insertMany(data)
-    // const dummyDataCreated = await Restaurant.insertMany(data)
-    const dummyDataCreated = await Tutorial.insertMany(data)
+    const dummyDataCreated = await type.insertMany(data)
     if (dummyDataCreated) {
-      const log = {
-        user_id: 'admin',
-        model: 'deal',
-        event_type: 'Dummy type Created',
-        reference_id: 'dummy-data',
-      }
-      // addLog(log)
-      console.log(process.env.MONGO_URI)
-      console.log(`${type}s created in local Mongo DB`)
-      // const allData = await type.find() // to get all the data back
-      // console.log(`'All ${type} -> `, allData) // to get all the data back
+      // const log = {
+      //   user_id: 'admin',
+      //   model: 'deal',
+      //   event_type: 'Dummy type Created',
+      //   reference_id: 'dummy-data',
+      console.log(`${name} => where created in local Mongo DB`)
     }
+    // addLog(log)
+    // console.log(`'All ${type} -> `, allData) // to get all the data back
+    // }
   } catch (error) {
     console.log('error -> ', error)
   }
 }
 
-// Promise.all([seedDB(dummyRestaurants, Restaurant), seedDB(dummyHomework, Homework)]).then(() =>
-//   mongoose.connection.close()
-// )
-
-// seedDB(dummyRestaurants, 'Restaurant').then(() => mongoose.connection.close())
-// seedDB(dummyHomework, 'Homework').then(() => mongoose.connection.close())
-seedDB(dummyTutorials, 'Tutotials').then(() => mongoose.connection.close())
+Promise.all([
+  seedDB(restaurantData, RestaurantModel, 'Restaurants'),
+  seedDB(tutorialsData, TutorialModal, 'Tutorials'),
+  seedDB(homeworkData, HomeworkModal, 'Homeworks'),
+  seedDB(jobsData, JobsModal, 'Jobs'),
+  seedDB(labsData, LabsModal, 'Labs'),
+  seedDB(lecturesData, LectureModel, 'Lectures'),
+  seedDB(newsData, NewsModel, 'News'),
+  seedDB(eventsData, EventModel, 'Events'),
+  seedDB(challengesData, ChallengeModel, 'Events'),
+]).then(() => mongoose.connection.close())
