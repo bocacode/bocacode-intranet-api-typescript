f183d44e3e9800a0e40f6e9bc7b96d27d49f6796
Ralph Sébastien Placide <placideralph@gmail.com>
2023-06-24 14:32:16 -0400

    update alumnis

--- src/controllers/alumniController.ts
+++ src/controllers/alumniController.ts
@@ -5,59 +5,62 @@ import Alumni from '../models/alumniModel'
 import { addLog } from './logController'
 
 export const addAlumni: RequestHandler = async (req, res) => {
-  if (req.method === 'POST' && req.body) {
-    try {
-      const duplicateAlumni = req.body.uid ? await Alumni.findOne({ uid: req.body.uid }) : null
-      if (duplicateAlumni) {
-        return res.status(401).json({ error: 'Alumni already in system' })
-      }
+  if (req.method !== 'POST') return res.status(401).json({ error: 'Invalid request method' })
 
-      const newAlumni = { ...req.body, uid: createRandomId() }
-
-      const alumniCreated = await Alumni.create(newAlumni)
-
-      if (alumniCreated) {
-        const log = {
-          user_id: req.body.created_by,
-          model: 'alumni',
-          event_type: 'new',
-          reference_id: newAlumni.uid,
-        }
-        addLog(log)
-
-        res.status(200).send('Alumni created')
-      } else {
-        res.status(401).json({ error: 'Deal was not created' })
-      }
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: 'Invalid data or HTTP method' })
+  try {
+    const duplicateAlumni = req.body.uid ? await Alumni.findOne({ uid: req.body.uid }) : null
+    if (duplicateAlumni) {
+      return res.status(401).json({ error: 'Alumni already in system' })
     }
+
+    const { user_id, ...restReqBody } = req.body;
+
+    const newAlumni = { ...restReqBody, uid: createRandomId(), created_by: user_id }
+
+    const alumniCreated = await Alumni.create(newAlumni)
+
+    if (alumniCreated) {
+      const log = {
+        user_id,
+        model: 'alumni',
+        event_type: 'new',
+        reference_id: newAlumni.uid,
+      }
+      addLog(log)
+
+      res.status(200).send(alumniCreated)
+    } else {
+      res.status(401).json({ error: 'Alumni was not created' })
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Invalid data or HTTP method' })
   }
 }
 
 export const updateAlumni: RequestHandler = async (req, res) => {
-  if (req.body) {
-    try {
-      const alumniUpdated = await Alumni.findOneAndUpdate({ uid: req.body.uid }, { $set: req.body })
+  if (req.method !== 'PATCH') return res.status(401).json({ error: 'Invalid request method' })
+  if (!req.params) return res.status(401).json({ message: 'Unable to update Alumni' })
 
-      if (alumniUpdated) {
-        const log = {
-          user_id: req.body.created_by,
-          model: 'alumni',
-          event_type: 'updated',
-          reference_id: alumniUpdated.uid,
-        }
-        addLog(log)
+  try {
+    const { user_id, ...restReqBody } = req.body
+    const alumniUpdated = await Alumni.findOneAndUpdate({ uid: req.params.id }, { $set: { ...restReqBody, created_by: user_id } }, { new: true })
 
-        res.status(200).send(alumniUpdated)
+    if (alumniUpdated) {
+      const log = {
+        user_id,
+        model: 'alumni',
+        event_type: 'updated',
+        reference_id: alumniUpdated.uid,
       }
-    } catch (err) {
-      res.status(500).send({ error: err })
+      addLog(log)
+
+      res.status(200).send(alumniUpdated)
+    } else {
+      res.status(401).json({ error: 'Alumni was not updated' })
     }
-  } else if (Error) {
-    console.log(Error)
-    res.status(401).send({ error: 'Update not completed or Access Denied' })
+  } catch (err) {
+    res.status(500).send({ error: err })
   }
 }
 
@@ -71,22 +74,20 @@ export const getAlumnis: RequestHandler = async (req, res) => {
 }
 
 export const getAlumni: RequestHandler = async (req, res) => {
-  if (req.params) {
+  if (!req.params) return res.status(401).json({ message: 'Unable to update Alumni' })
+  
+  try {
     const { id } = req.params
-    try {
-      const userFound = await Alumni.findById(id)
-      res.status(200).send(userFound)
-    } catch (err) {
-      res.status(500).send({ error: err })
-    }
-  } else if (Error) {
-    console.log(Error)
-    res.status(401).send({ error: 'Update not completed or Access Denied' })
+    const userFound = await Alumni.findOne({ uid: id })
+    res.status(200).send(userFound)
+  } catch (err) {
+    res.status(500).send({ error: err })
   }
 }
 
 export const disableAlumni: RequestHandler = async (req, res) => {
-  if (!req.params) return res.status(401).send({ error: 'Update not completed or Access Denied' })
+  if (req.method !== 'PATCH') return res.status(401).json({ error: 'Invalid request method' })
+  if (!req.params) return res.status(401).json({ message: 'Unable to update Alumni' })
 
   try {
     const { id } = req.params
@@ -101,6 +102,8 @@ export const disableAlumni: RequestHandler = async (req, res) => {
       }
       addLog(log)
       res.status(200).send({ success: `Alumni id ${id} has been disabled ` })
+    } else {
+      res.status(401).send({ error: `Alumni was not disabled` })
     }
   } catch (err) {
     res.status(500).send({ error: err })
