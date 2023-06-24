2b88f6c67202a150de0d341642eb050ebaa5c982
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-06-24 18:54:36 -0400

    fixed error on cohortroutes forgetting to add export default router

--- src/routes/cohortRoutes.ts
+++ src/routes/cohortRoutes.ts
@@ -10,3 +10,5 @@ router.route('/:id').get(auth, getCohort)
 router.route('/').post(auth, checkRequestBody, addCohort)
 router.route('/disable/:id').delete(auth, checkRequestBody, disableCohort)
 router.route('/:id').patch(auth, checkRequestBody, updateCohort)
+
+export default router
