f3ab0c5327e0b3f6e149d49a83939f528ebe8dc1
Idan Fischer <Idanfischer5@gmail.com>
2023-06-24 13:59:01 -0400

    added some failsafes

--- src/controllers/tutorialsController.ts
+++ src/controllers/tutorialsController.ts
@@ -5,55 +5,60 @@ import Tutorials from '../models/tutorialModel'
 import { addLog } from './logController'
 
 export const addTutorial: RequestHandler = async (req, res) => {
-  if (req.method !== 'POST') return res.status(401).json({ error: 'Invalid HTTP method' })
-  try {
-    const duplicateTutorial = req.body.uid ? await Tutorials.findOne({ uid: req.body.uid }) : null
-    if (duplicateTutorial) {
-      return res.status(401).json({ error: 'Tutorial already in system' })
-    }
-
-    const newTutorial = { ...req.body, uid: createRandomId() }
-
-    const tutorialCreated = await Tutorials.create(newTutorial)
-
-    if (tutorialCreated) {
-      const log = {
-        user_id: req.body.user_id,
-        model: 'tutorial',
-        event_type: 'new',
-        reference_id: newTutorial.uid,
+  if (req.method === 'POST' && req.body) {
+    try {
+      const duplicateTutorial = req.body.uid ? await Tutorials.findOne({ uid: req.body.uid }) : null
+      if (duplicateTutorial) {
+        return res.status(401).json({ error: 'Tutorial already in system' })
       }
-      addLog(log)
 
-      res.status(200).send('Tutorial created')
-    } else {
-      res.status(401).json({ error: 'Tutorial was not created' })
+      const newTutorial = { ...req.body, uid: createRandomId() }
+
+      const tutorialCreated = await Tutorials.create(newTutorial)
+
+      if (tutorialCreated) {
+        const log = {
+          user_id: req.body.user_id,
+          model: 'tutorial',
+          event_type: 'new',
+          reference_id: newTutorial.uid,
+        }
+        addLog(log)
+
+        res.status(200).send('Tutorial created')
+      } else {
+        res.status(401).json({ error: 'Tutorial was not created' })
+      }
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ error: 'Invalid data or HTTP method' })
     }
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: 'Invalid data or HTTP method' })
   }
 }
 
 export const updateTutorial: RequestHandler = async (req, res) => {
-  if (!req.params) return res.status(401).json({ error: 'Unable to update tutorial' })
-  try {
+  if (req.body && req.params) {
     const { id } = req.params
-    const tutorialUpdated = await Tutorials.findOneAndUpdate({ uid: id }, { $set: req.body }, { new: true })
+    try {
+      const tutorialUpdated = await Tutorials.findOneAndUpdate({ uid: id }, { $set: req.body }, { new: true })
 
-    if (tutorialUpdated) {
-      const log = {
-        user_id: req.body.user_id,
-        model: 'tutorial',
-        event_type: 'updated',
-        reference_id: tutorialUpdated.uid,
+      if (tutorialUpdated) {
+        const log = {
+          user_id: req.body.user_id,
+          model: 'tutorial',
+          event_type: 'updated',
+          reference_id: tutorialUpdated.uid,
+        }
+        addLog(log)
+
+        res.send(tutorialUpdated)
       }
-      addLog(log)
-
-      res.send(tutorialUpdated)
+    } catch (err) {
+      res.status(500).send({ error: err })
     }
-  } catch (err) {
-    res.status(500).send({ error: err })
+  } else if (Error) {
+    console.log(Error)
+    res.status(401).send({ error: 'Update not completed or Access Denied' })
   }
 }
 
@@ -67,13 +72,14 @@ export const getTutorials: RequestHandler = async (req, res) => {
 }
 
 export const getTutorial: RequestHandler = async (req, res) => {
-  if (!req.params) return res.status(401).send({ error: 'ID missing or Access Denied' })
-  try {
+  if (req.params) {
     const { id } = req.params
-    const tutorialFound = await Tutorials.findById(id)
-    res.status(200).send(tutorialFound)
-  } catch (err) {
-    res.status(500).send({ message: 'Unable to get Tutorial' })
+    try {
+      const tutorialFound = await Tutorials.findOne({ uid: id })
+      res.status(200).send(tutorialFound)
+    } catch (err) {
+      res.status(500).send({ message: 'Unable to get Tutorial' })
+    }
   }
 }
 
