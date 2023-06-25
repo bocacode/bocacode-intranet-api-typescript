0bfa2f33afa7f2d881f8386404a33d3d20da43af
jonathans199 <jso1919@gmail.com>
2023-06-25 12:10:53 -0400

    chore(cleanup): updated read me and seed scripts

--- src/scripts/seedDbClear.ts
+++ src/scripts/seedDbClear.ts
@@ -1,53 +1,34 @@
-import mongoose, { ConnectOptions } from 'mongoose'
+import mongoose from 'mongoose'
 import 'dotenv/config'
 
-import { addLog } from '../controllers/logController'
+import { mongooseConnect } from '../utils/mongoUtility'
 
 import RestaurantModel from '../models/restaurantModel'
-
 import TutorialModal from '../models/tutorialModel'
-
 import HomeworkModal from '../models/homeworkModel'
-
 import JobsModal from '../models/jobModel'
-
 import LabsModal from '../models/labModel'
-
 import LectureModel from '../models/lectureModel'
-
 import NewsModel from '../models/newsModel'
-
 import EventModel from '../models/eventModel'
-
 import ChallengeModel from '../models/codeChallengeModel'
+import studentModel from '../models/studentModel'
+import usersModel from '../models/usersModel'
+import logsModel from '../models/logsModel'
+import wellnessModel from '../models/wellnessModel'
+import cohortModel from '../models/cohortModel'
 
-mongoose.set('strictQuery', false)
-mongoose
-  .connect(
-    process.env.MONGO_URI as string,
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    } as ConnectOptions
-  )
-  .then((result: any) => {
-    if (result.STATES['1']) {
-      console.log(`Connected to Mongo -  ${process.env.PORT}`)
-    } else {
-      console.error
-    }
-  })
-  .catch((err) => console.error(err))
+mongooseConnect()
 
-const seedDB = async (type: any, name: string) => {
+const clearSeedData = async (type: any, name: string) => {
   try {
-    const mongoString = process.env.MONGO_URI as string;
-    if (!!mongoString && !mongoString.toLowerCase().includes('localhost')) return;
-    if (process.env.NODE_ENV && process.env.NODE_ENV !== 'development') return;
+    const mongoString = process.env.MONGO_URI as string
+    if (!!mongoString && !mongoString.toLowerCase().includes('localhost')) return
+    if (process.env.NODE_ENV && process.env.NODE_ENV !== 'development') return
 
     const dummyDataDeleted = await type.deleteMany({})
     if (dummyDataDeleted) {
-      console.log(`${name} => where deleted in local Mongo DB`)
+      console.log(`${name} => where deleted in LOCAL Mongo DB`)
     }
   } catch (error) {
     console.log('error -> ', error)
@@ -55,13 +36,20 @@ const seedDB = async (type: any, name: string) => {
 }
 
 Promise.all([
-  seedDB(RestaurantModel, 'Restaurants'),
-  seedDB(TutorialModal, 'Tutorials'),
-  seedDB(HomeworkModal, 'Homeworks'),
-  seedDB(JobsModal, 'Jobs'),
-  seedDB(LabsModal, 'Labs'),
-  seedDB(LectureModel, 'Lectures'),
-  seedDB(NewsModel, 'News'),
-  seedDB(EventModel, 'Events'),
-  seedDB(ChallengeModel, 'Events'),
-]).then(() => mongoose.connection.close())
+  clearSeedData(RestaurantModel, 'Restaurants'),
+  clearSeedData(TutorialModal, 'Tutorials'),
+  clearSeedData(HomeworkModal, 'Homeworks'),
+  clearSeedData(JobsModal, 'Jobs'),
+  clearSeedData(LabsModal, 'Labs'),
+  clearSeedData(LectureModel, 'Lectures'),
+  clearSeedData(NewsModel, 'News'),
+  clearSeedData(EventModel, 'Events'),
+  clearSeedData(ChallengeModel, 'Challenges'),
+  clearSeedData(studentModel, 'Students'),
+  clearSeedData(usersModel, 'Users'),
+  clearSeedData(logsModel, 'Logs'),
+  clearSeedData(wellnessModel, 'Wellness'),
+  clearSeedData(cohortModel, 'Cohorts'),
+])
+  .then(() => mongoose.connection.close())
+  .catch((err) => console.error(err))
